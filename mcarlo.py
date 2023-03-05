e4120f3827d14f1f2d12c9faf85c68bf55763af3
mirroredkube <anitha.praveenrao@gmail.com>
2023-03-05 16:41:01 +0100

    Monte carlo simulation initial commit

---
+++ mcarlo.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime as dt
+import yfinance as yf
+
+# Import data from yahoo finance
+
+def get_data(stocks, start, end):
+    stockData = yf.download(stocks, start, end)
+    stockData = stockData['Close']
+    returns = stockData.pct_change()
+    meanReturns = returns.mean()
+    covMatrix = returns.cov()
+    return meanReturns, covMatrix
+
+stockList = ['CBA', 'BHP', 'TLS', 'NAB', 'WBC', 'STO']
+stocks = [stock + '.AX' for stock in stockList]
+#stockList = ['TATAELXSI']
+#stocks = [stock + '.NS' for stock in stockList]
+endDate = dt.datetime.now()
+startDate = endDate - dt.timedelta(days=300)
+
+meanReturns, covMatrix = get_data(stocks, startDate, endDate)
+
+weights = np.random.random(len(meanReturns))
+weights /= np.sum(weights)
+
+# Monte Carlo Method
+mc_sims = 400 # number of simulations
+T = 100 #timeframe in days
+
+meanM = np.full(shape=(T, len(weights)), fill_value=meanReturns)
+meanM = meanM.T
+
+portfolio_sims = np.full(shape=(T, mc_sims), fill_value=0.0)
+
+initialPortfolio = 10000
+
+for m in range(0, mc_sims):
+    Z = np.random.normal(size=(T, len(weights)))# uncorrelated RV's
+    L = np.linalg.cholesky(covMatrix) # Cholesky decomposition to Lower Triangular Matrix
+    dailyReturns = meanM + np.inner(L, Z) # Correlated daily returns for individual stocks
+    portfolio_sims[:,m] = np.cumprod(np.inner(weights, dailyReturns.T)+1)*initialPortfolio
+
+plt.plot(portfolio_sims)
+plt.ylabel('Portfolio Value ($)')
+plt.xlabel('Days')
+plt.title('MC simulation of a stock portfolio')
+plt.show()
